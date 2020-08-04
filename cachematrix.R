bfa99e27510aedf22ee8b7de92d6d41184e79ae6
Felipe Villota <villotamacias@gmail.com>
2020-08-03 19:38:54 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## With this two functions we will show how the state inside of an object is 
+## preserved.
+## by FELIPE VILLOTA
 
-## Write a short comment describing this function
+
+## The following function creates a special "matrix" object that can
+## cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        set1 <- function(inverse) m <<- inverse
+        get1 <- function() m
+        list(set = set, get = get,
+             set1 = set1,
+             get1 = get1)
 }
 
 
-## Write a short comment describing this function
+## This function will retrieve the inverse of makeCacheMatrix 
+## (if the object hasn't changed).
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        m<- x$get1()
+        if(!is.null(m)) {
+                message("cached computation")
+                return(m)
+        }
+        
+        z<- x$get()
+        m<- solve(z,...)
+        x$set1(m)
+        m
+        
+        
+        
 }
